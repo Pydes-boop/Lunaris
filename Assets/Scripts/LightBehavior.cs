42fb55d42547f9a17c507b845e8ba52e91e700c6
Ather_Weather <marcelstaniszewski@gmx.de>
2020-11-28 16:35:36 +0100

    Licht und Taschenlame erstes Scrpite hinzugefügt

---
+++ Assets/Scripts/LightBehavior.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LightBehavior : MonoBehaviour
+{
+    public GameObject lightGameObject;
+    private Light lightComp;
+    public GameObject lightGameObject2;
+    private Light lightComp2;
+
+    int counter=0;
+    bool switcher = false;
+
+    void Start()
+    {
+        lightComp = lightGameObject.GetComponent<Light>();
+        lightComp2 = lightGameObject2.GetComponent<Light>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {/*
+        counter++;
+        if (counter > 240)
+        {
+            lightComp.enabled = switcher;
+            lightComp2.enabled = switcher;
+            counter = 0;
+            switcher = !switcher;
+        }
+        */
+    }
+}
---
+++ Assets/Scripts/TorchBehavior.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TorchBehavior : MonoBehaviour
+{
+    public GameObject lightGameObject;
+    private Light lightComp;
+    private int counter = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        // Add the light component
+       lightComp = lightGameObject.GetComponent<Light>();
+        //lightComp.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {   //Change the collor of the Light
+        //lightComp.color = Color.blue;
+        //Change the intensity of Light
+        //lightComp.intensity -= 1;
+        //Slowly change the intensity of the Light
+       /* if(lightComp.intensity > 0)
+        {
+            lightComp.intensity -= 0.05f;
+        }*/
+        //Shows the light or hides it (On,OFF)
+        //lightComp.enabled = false;
+        /*
+        counter++;
+        if(counter >= 250)
+        {
+            lightComp.enabled = true;
+        }*/
+
+
+
+
+    }
+}
