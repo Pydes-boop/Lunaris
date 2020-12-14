65482c608c700cf5e25b3a78cf75cd82c5662732
ge93luc <ge93luc@mytum.de>
2020-12-14 15:35:43 +0100

    gravity

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -9,11 +9,19 @@ public class PlayerMovement : MonoBehaviour
 {
     public CharacterController controller;
     public float speed = 12f;
+    public float gravity = 9.81f;
+    private float verticalVelocity;
     // Update is called once per frame
     void Update()
     {
+        if (!controller.isGrounded)
+        {
+            verticalVelocity -= gravity * Time.deltaTime;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
+        Vector3 grav = new Vector3(0, verticalVelocity, 0);
+        controller.Move(grav * Time.deltaTime);
         Vector3 move = transform.right * x + transform.forward * z;
         controller.Move(move * speed * Time.deltaTime);
     }
