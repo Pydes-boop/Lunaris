0813791029c205b77c7a5f34cb8deaea99b0eee0
ge93luc <ge93luc@mytum.de>
2021-01-02 16:03:17 +0100

    Diary mit string liste (behindert)

---
+++ Assets/Scripts/PlayerDex.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+public class PlayerDex : MonoBehaviour
+{
+    private List<String> diaries = new List<String>();
+    private bool open = true;
+    private string text = null;
+    int index = 0;
+    //index wird default auf 0 gesetzt;
+    void Start()
+    {
+        diaries.Add("sdhfsuiodfhsdiufh\nsaiduhfuiwehfiuwehf\naiougheiuorgherg\n");
+        diaries.Add("ioauehgiuerhgiuherg\nsaiduhfuiwehfiuwehf\nasuxucghsdduihf\n");
+    }
+    public void addPage(String input)
+    {
+        diaries.Add(input);
+    }
+    string getNext()
+    {
+        if (index + 1 <= diaries.Count)
+        {
+            index++;
+        }
+        return diaries.ElementAt(index);
+    }
+    string getPrev()
+    {
+        if(index-1 >= 0)
+        {
+            index--;
+        }
+        return diaries.ElementAt(index);
+    }
+    void OnGUI()
+    {
+        if (open)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            GUI.Box(new Rect(0, 0, 320, 455), "");
+            GUI.Box(new Rect(0, 0, 320, 455), text);
+            if (GUI.Button(new Rect(110, 350, 100, 100), "Exit"))
+			{
+               open = false;
+               Cursor.lockState = CursorLockMode.Locked;
+            }
+		    if (GUI.Button(new Rect(215, 350, 100, 100), "Next"))
+			{
+               text = getNext();
+			}
+			if (GUI.Button(new Rect(5, 350, 100, 100), "Prev"))
+			{
+               text = getPrev();
+			}
+		}
+    }
+}
---
+++ Assets/Scripts/TextPage.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class TextPage : MonoBehaviour
+{
+	private bool onTrigger = false;
+	private bool collected = false;
+	public int first, second;
+	public int fx, fy;
+	void OnTriggerEnter(Collider other)
+	{
+		onTrigger = true;
+	}
+
+	void OnTriggerExit(Collider other)
+	{
+		onTrigger = false;
+	}
+    void OnGUI()
+    {
+		if (onTrigger)
+		{
+			if (!collected)
+			{
+				GUI.Box(new Rect(0, 0, 200, 25), "Press 'E' to collect Paper");
+
+				if (Input.GetKeyDown(KeyCode.E))
+				{
+					collected = true;
+				}
+			}
+		}
+        if (collected)
+        {
+			Cursor.lockState = CursorLockMode.None;
+			GUI.Box(new Rect(0, 0, 320, 455), "");
+		}
+		
+	}
+}
