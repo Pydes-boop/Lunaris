ad062de76cce00a48bba65c5795c26283292d7c1
Daniel Klitzner <daniel.klitzner@yahoo.com>
2021-01-04 16:50:29 +0100

    Added Random Horror Sound Player

---
+++ Assets/Scripts/AudioSourceRandom.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioSourceRandom : MonoBehaviour
+{
+
+    public AudioSource randomSound;
+
+    public AudioClip[] audioSources;
+
+    private int random;
+
+    // Use this for initialization
+    void Start()
+    {
+        random = 1;
+        CallAudio();
+    }
+
+
+    void CallAudio()
+    {
+        Invoke("RandomSoundness", 5);
+    }
+
+    void RandomSoundness()
+    {
+        Debug.Log("It works");
+        randomSound.clip = audioSources[Random.Range(0, audioSources.Length)];
+        if (Random.Range(0, 4) == 1)
+        {
+            randomSound.Play();
+        }
+        CallAudio();
+    }
+}
