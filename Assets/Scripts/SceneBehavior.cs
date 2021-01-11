c2d241de62b749d6d40f00f974712822adc22fab
Ather_Weather <marcelstaniszewski@gmx.de>
2021-01-11 21:14:46 +0100

    Übergänge erstellt

---
+++ Assets/Scripts/SceneBehavior.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class SceneBehavior : MonoBehaviour
+{
+   public void changeSceneTo(int sceneIndex)
+    {
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void closeApplication()
+    {
+        Application.Quit();
+    }
+}
