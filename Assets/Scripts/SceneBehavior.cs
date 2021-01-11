36aee8236ba0ace448612a9289376ea35f577d5c
Ather_Weather <marcelstaniszewski@gmx.de>
2021-01-11 21:40:34 +0100

    Endzone hinzugefügt

--- Assets/Scripts/SceneBehavior.cs
+++ Assets/Scripts/SceneBehavior.cs
@@ -6,6 +6,10 @@ using UnityEngine.UI;
 
 public class SceneBehavior : MonoBehaviour
 {
+   public int levelIndex;
+    public bool curserLocked = false;
+    public bool curserVisible = true;
+
    public void changeSceneTo(int sceneIndex)
     {
         SceneManager.LoadScene(sceneIndex);
@@ -15,4 +19,27 @@ public class SceneBehavior : MonoBehaviour
     {
         Application.Quit();
     }
+
+    public void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(levelIndex);
+            setCurser();
+        }
+    }
+
+    void setCurser()
+    {
+        if (!curserLocked)
+        {
+            Cursor.lockState = CursorLockMode.None;
+        }
+        else
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+        }
+            Cursor.visible = curserVisible;        
+    }
+
 }
