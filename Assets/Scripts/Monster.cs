46158ff667f86d5a917af7d8beedfa8c155e8bc7
ge93luc <ge93luc@mytum.de>
2021-01-14 16:13:58 +0100

    Monster test

---
+++ Assets/Scripts/Monster.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    float maxDistance = 10;
+    void Start()
+    {
+    }
+    void FixedUpdate()
+    {
+
+        // Will contain the information of which object the raycast hit
+        RaycastHit hit;
+
+        // if raycast hits, it checks if it hit an object with the tag Player
+        if (Physics.Raycast(transform.position, transform.forward, out hit, maxDistance) &&
+                    hit.collider.gameObject.CompareTag("Player"))
+
+        {
+            Debug.Log("nice");
+            // Starts the countdown to destroy the enemy
+            StartCoroutine(Scare());
+
+        }
+
+    }
+
+
+
+    IEnumerator Scare()
+    {
+        this.transform.Translate(4,0,0);
+        yield return new WaitForSeconds(3);
+        this.transform.Translate(-4, 0, 0);
+    }
+}
